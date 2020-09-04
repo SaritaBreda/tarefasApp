03b8de6018da27060cf7e527cf06a38e7ce4da3d
Sarita Breda Gabani <sarita.gabani@etec.sp.gov.br>
2020-09-04 17:20:33 -0300

    registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -4,6 +4,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CpfValidator } from '../validators/cpf-validator';
 import { ComparacaoValidator } from '../validators/comparacao-validator';
+import { UsuariosService } from '../services/usuarios.service';
+import { AlertController } from '@ionic/angular';
+import { Usuario } from '../models/Usuario';
 
 @Component({
   selector: 'app-registro',
@@ -57,7 +60,12 @@ export class RegistroPage implements OnInit {
   };
 
 
-  constructor(private formBuilder: FormBuilder, private router:Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router:Router,
+    private usuariosService: UsuariosService,
+    public alertController: AlertController) {
+
     this.formRegistro = formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf: ['', Validators.compose([Validators.required, Validators.minLength(11), Validators.maxLength(14), CpfValidator.cpfValido])],
@@ -73,7 +81,9 @@ export class RegistroPage implements OnInit {
    }
 
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.usuariosService.buscarTodos();
+    console.log(this.usuariosService.listaUsuarios);
   }
 
   public registro() {
@@ -82,6 +92,40 @@ export class RegistroPage implements OnInit {
     } else {
       console.log('Formulário inválido!');
     }
+  }
+
+  public async salvarFormulario() {
+    if(this.formRegistro.valid) {
+
+      let usuario = new Usuario();
+      usuario.nome = this.formRegistro.value.nome;
+      usuario.cpf = this.formRegistro.value.cpf;
+      usuario.dataNascimento = new Date(this.formRegistro.value.dataNascimento);
+      usuario.genero = this.formRegistro.value.genero;
+      usuario.celular = this.formRegistro.value.celular;
+      usuario.email = this.formRegistro.value.email;
+      usuario.senha = this.formRegistro.value.senha;
+
+      if(await this.usuariosService.salvar(usuario)) {
+        this.exibirAlerta('SUCESSO!', 'Usuário salvo com sucesso!');
+        this.router.navigateByUrl('/login');
+      } else {
+        this.exibirAlerta('ERRO!', 'Erro ao salvar usuário!');
+      }
+
+    } else {
+      this.exibirAlerta('ADVERTENCIA!', 'Formulário inválido<br/>Verifique os campos do seu formulário!');
+    }
+  }
+
+  async exibirAlerta(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
